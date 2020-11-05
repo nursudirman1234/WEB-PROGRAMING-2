1a1b747f962ba81f9c442a0bfe562618d8e45024
Nur sudirman al afgani <64182909+nursudirman1234@users.noreply.github.com>
2020-11-05 16:05:11 +0700

    Add files via upload

---
+++ rental/views/_partials/footer.php
@@ -0,0 +1,8 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span>
+				&copy; Aplikasi Rental Mobil <?= date('Y') ?></span>
+		</div>
+	</div>
+</footer>
